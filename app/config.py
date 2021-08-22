7e0634f7fc5ee4b600dcdd8f9e7260e7547123f6
saravanan palanisamy <saravanan30erd@gmail.com>
2021-08-22 20:06:19 +0400

    add logging

---
+++ app/config.py
@@ -0,0 +1,15 @@
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+VIRTSERVERS = [
+    '10.0.10.1:9090',
+    '10.0.10.2:9090'
+]
